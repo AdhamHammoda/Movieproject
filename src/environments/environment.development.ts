7a6964b70579c0b9090ada30b4693c42688f87ff
Adham Hamouda <a.hammoda@nu.edu.eg>
2023-09-12 16:12:52 +0300

    Added Environments

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    domain: 'https://api.themoviedb.org/3/movie/top_rated?api_key=6dde6f39000cee201e16d8d224b8a850',
+    imagePaths : 'https://image.tmdb.org/t/p/w500/',
+    moviesPaths: 'https://api.themoviedb.org/3/movie/',
+    apiKey:  '6dde6f39000cee201e16d8d224b8a850'
+};
